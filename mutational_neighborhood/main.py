816d1b498929a37bfa06a691dcde740a62fa1974
MutationalNeighborhoods <getlethality@gmail.com>
2021-09-28 12:00:22 -0700

    added get mutational neighborhood

---
+++ mutational_neighborhood/main.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+from geneticAlgorithm.library import library
+from geneticAlgorithm.fitnessFunctions import fitnessFunctions
+from classes.Encoding import Encoding
+import random
+import geneticAlgorithm.constants as constants
+
+def generateMutated(encoder, trap, mutationFunction = library.mutationFuc, numMutants = 10):
+    """Generates a list of possible mutated traps produced from mutating the same trap"""
+    listMutants = [None for _ in range(numMutants)]
+    for i in range(numMutants):
+        listMutants[i] = mutationFunction(encoder, trap)
+    return listMutants
+
+
+def computeChanges(encoder,trap,mutatedTraps):
+    """Compares the coherence and lethality of the mutated traps to the original trap"""
+    newCoherences = [0 for _ in range(len(mutatedTraps))]
+    newLethalities = [0 for _ in range(len(mutatedTraps))]
+
+    for i in range(len(mutatedTraps)):
+        newCoherences[i], newLethalities[i] = getCoherenceAndLethality(encoder, mutatedTraps[i])
+    
+    return newCoherences, newLethalities
+
+def controlledSubstitution(location, encoding: Encoding, trap):
+    """Perform a substitution mutation at a specified and valid location in the trap encoding"""
+    if location in (encoding.food, encoding.floor, encoding.door):
+        raise ValueError("Location must be part of trap encoding")
+    trap[location] = constants.CELL_ALPHABET[random.randrange(2, len(constants.CELL_ALPHABET), 1)]
+
+def getMutationalNeighborhoodStatistics(encoder, trap): 
+    mutatedTraps = generateMutated(encoder, trap)
+    newCoherences, newLethalities = computeChanges(encoder, trap,mutatedTraps)
+    return mutatedTraps,newCoherences,newLethalities
+
+def getCoherenceAndLethality(encoder, trap):
+    """Returns both the coherence and lethality of a trap"""
+    encodedTrap = Encoding.encode(trap)
+    coherence = fitnessFunctions.getCoherence(encodedTrap,encoder)   
+    lethality = fitnessFunctions.getLethalty(encodedTrap,encoder)
+    return coherence,lethality
+
+def randomMutation(encoding: Encoding, trap):
+    """Performs a mutation that is one of: substitution, deletion, insertion"""
+    mutationType = "sub"
+    rand1 = random.randrange(0, 1) * 3
+    if rand1 > 1:
+        mutationType = "del"
+    elif rand1 > 2:
+        mutationType = "ins"
+    
+    if mutationType == "sub":
+        return library.mutationFunc(encoding, trap)
+
+    elif mutationType == "del":
+        index = random.randrange(0, len(trap), 1)
+        while index in (encoding.food, encoding.floor, encoding.door):
+            index = random.randrange(0, len(trap), 1)
+
+def main():
+    trap = library.generateTrap()
+    encoder = Encoding()
+    coherence,lethality = getCoherenceAndLethality(encoder, trap)
+    print(getMutationalNeighborhoodStatistics(encoder, trap))
+    
+
+if __name__ == "__main__":
+    main()
+    
+
+
